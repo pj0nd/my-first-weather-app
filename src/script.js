14bbafc4af6beffc00736bba39f8ef314fcabb6f
pj0nd <110718718+pj0nd@users.noreply.github.com>
2022-10-05 14:37:26 +0300

    farenheit conversion

--- src/script.js
+++ src/script.js
@@ -27,47 +27,24 @@ function formatDay(timestamp) {
 
   return days[day];
 }
-function displayForecast(response) {
-  let forecast = response.data.daily;
-  let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="row">`;
 
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-    <div class="col-2">
-      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-      <img
-        src="http://openweathermap.org/img/wn/${
-          forecastDay.weather[0].icon
-        }@2x.png"
-        alt=""
-        width="55"
-      />
-      <div class="weather-forecast-temp">
-        <span class="weather-forcast-temp-max"> ${Math.round(
-          forecastDay.temp.max
-        )}° </span>
-        <span class="weather-forecast-temp-min">${Math.round(
-          forecastDay.temp.min
-        )}°</span>
-      </div>
-    </div>`;
-    }
-  });
+function convertToFahr(event) {
+  event.preventDefault();
+  let tempCel = document.querySelector("#currentTemp");
+  let fahr = (celsius * 9)/5+32;
+  tempCel.innerHTML = `${Math.round(fahr)}°F`;
+  celsiusConverter.classList.remove("active");
+  fahrConverter.classList.add("active");
+}
 
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
+function convertToCelsius(event) {
+  event.preventDefault();
+  let celsiusTemp = document.querySelector("#currentTemp");
+  celsiusTemp.innerHTML = `${Math.round(celsius)}°C`;
+  fahrConverter.classList.remove("active");
+  celsiusConverter.classList.add("active");
 }
-function getForecast(coordinates) {
-  let apiKey = "1fd12663f5175c8f043543ba07cbd1c5";
-  let lon = coordinates.lon;
-  let lat = coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
+
 function displayTemp(response) {
   let currentTemp = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
